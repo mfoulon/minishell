bf04f983984e4707fa66f96f5d65a7dcfaffe15f
Martin FOULON <mafoulon@paul-f5Ar5s3.clusters.42paris.fr>
2023-12-07 14:22:45 +0100

    organised dirs

---
+++ includes/minishell.h
@@ -0,0 +1,46 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <termios.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft/libft.h"
+
+// Tokens
+
+typedef enum e_token_type
+{
+	T_IDENTIFIER,
+	T_LESS,
+	T_GREAT,
+	T_DLESS,
+	T_DGREAT,
+	T_PIPE,
+	T_O_PARENT,
+	T_C_PARENT,
+	T_AND,
+	T_OR,
+	T_NL,
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type		type;
+	char				*value;
+	struct s_token		*next;
+	struct s_token		*prev;
+}	t_token;
+
+
+#endif
