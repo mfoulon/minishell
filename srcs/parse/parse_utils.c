ec2099aba5a55c229730279c3d358e200b5b2085
Baptiste Vieilhescaze <baptistevieilhescaze@MacBook-Air-de-Baptiste.local>
2023-12-14 15:17:27 +0100

    parse part started

--- srcs/parse/parse_utils.c
+++ srcs/parse/parse_utils.c
@@ -1,12 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: baptistevieilhescaze <baptistevieilhesc    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/14 15:03:08 by baptistevie       #+#    #+#             */
-/*   Updated: 2023/12/14 15:03:40 by baptistevie      ###   ########.fr       */
+/*   Created: 2023/12/14 15:04:01 by baptistevie       #+#    #+#             */
+/*   Updated: 2023/12/14 15:08:35 by baptistevie      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+t_bool	is_binoperand(t_token token)
+{
+	if (!token)
+		return (false);
+	return (token->token_type == T_PIPE
+			|| token->token_type == T_AND
+			|| token->token_type == T_OR)
+}
 
