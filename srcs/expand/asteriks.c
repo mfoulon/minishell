a560f7b760735575b8836c0cc367c8d22c38f1a1
Martin FOULON <mafoulon@paul-f5Ar4s5.clusters.42paris.fr>
2023-12-14 15:21:38 +0100

    handle asterisks

--- srcs/expand/asteriks.c
+++ srcs/expand/asteriks.c
@@ -1,6 +1,6 @@
 #include "../../includes/minishell.h"
 
-static void	ft_handle_pattern_quotes(char **pattern, char *quotes)
+static void	handle_pattern_quotes(char **pattern, char *quotes)
 {
 	if (**pattern == '"' || **pattern == '\'')
 	{
@@ -15,4 +15,56 @@ static void	ft_handle_pattern_quotes(char **pattern, char *quotes)
 			}
 		}
 	}
+}
+
+static bool	handle_stars(char **pattern, char **l_wildcard, char **l_match, char *s)
+{
+	while (**pattern == '*')
+		(*pattern)++;
+	if (!**pattern)
+		return (true);
+	*l_wildcard = *pattern;
+	*l_match = s;
+	return (false);
+}
+
+static bool is_pattern_match(char **pattern, char **l_wildcard, char **l_match, char **s)
+{
+	if (**pattern == **s)
+	{
+		(*pattern)++;
+		(*s)++;
+		return (true);
+	}
+	else if (*l_wildcard)
+	{
+		*s = ++(*l_match);
+		*pattern = *l_wildcard;
+		return (true);
+	}
+	else
+		return (false);
+}
+
+bool	is_star_match(char *pattern, char *s)
+{
+	char 	*l_wildcard;
+	char	*l_match;
+	char	quotes;
+
+	quotes = 0;
+	l_match = NULL;
+	l_wildcard = NULL;
+	while (*s)
+	{
+		handle_pattern_quotes(&pattern, &quotes);
+		if (*pattern == '*' && !quotes && handle_stars(&pattern, &l_wildcard, &l_match, s))
+			return (true);
+		else if (!is_pattern_match(&pattern, &l_wildcard, &l_match, &s))
+			return (false);
+	}
+	if (*pattern == '*')
+		while (*pattern == '*')
+			pattern++;
+	return(!*pattern);
 }
